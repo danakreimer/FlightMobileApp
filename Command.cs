0e544cb729727d4e5dcbb5a0de35edf30d7f66e7
danak1997 <danathekraimer@gmail.com>
2020-06-15 15:01:32 +0300

    added the kotlin android app directory

---
+++ Command.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightMobileApp.Models
+{
+	public class Command
+	{
+		public Command(double aileron, double throtle, double rudder, double elevator)
+		{
+			Aileron = aileron;
+			Throtle = throtle;
+			Rudder = rudder;
+			Elevator = elevator;
+		}
+
+		[JsonProperty("aileron")]
+		public double Aileron { get; set; }
+		[JsonProperty("rudder")]
+		public double Rudder { get; set; }
+		[JsonProperty("elevator")]
+		public double Elevator { get; set; }
+		[JsonProperty("throttle")]
+		public double Throttle { get; set; }
+	}
+}
---
+++ ICommandManager.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class ICommandManager
+{
+	public ICommandManager()
+	{
+	}
+}
