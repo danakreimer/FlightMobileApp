d4d46752f49d444306ab7cfacb4dc99692911de9
maybachar <mayb1995@gmail.com>
2020-06-15 20:40:07 +0300

    Create CommandController.cs

---
+++ FlightMobileApp/Controllers/CommandController.cs
@@ -0,0 +1,36 @@
+﻿using FlightMobileApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightMobileApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly ICommandManager commandManager;
+        // Constructor
+        public CommandController(ICommandManager commandManager)
+        {
+            this.commandManager = commandManager;
+        }
+
+        // POST: api/command
+        [HttpPost(Name = "SendCommandToSimulator")]
+        public void SendCommandToSimulator([FromBody] Command command)
+        {
+            try
+            {
+                commandManager.SendCommand(command);
+                // TODO: return success status///////////////////////////////////////////////////
+            }
+            catch (Exception)
+            {
+                // TODO: return failier status./////////////////////////////////////////////////
+            }
+        }
+    }
+}
