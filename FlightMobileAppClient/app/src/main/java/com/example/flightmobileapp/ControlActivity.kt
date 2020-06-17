4577ab1e04c3c64623f97511921a6a841b1f70ad
danak1997 <danathekraimer@gmail.com>
2020-06-17 12:07:08 +0300

    add joystick

--- FlightMobileAppClient/app/src/main/java/com/example/flightmobileapp/ControlActivity.kt
+++ FlightMobileAppClient/app/src/main/java/com/example/flightmobileapp/ControlActivity.kt
@@ -2,10 +2,16 @@ package com.example.flightmobileapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.controlwear.virtual.joystick.android.JoystickView
+
 
 class ControlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.control_screen)
+        val joystick = findViewById<JoystickView>(R.id.joystickView)
+        joystick.setOnMoveListener { angle, strength ->
+            // do whatever you want
+        }
     }
 }
