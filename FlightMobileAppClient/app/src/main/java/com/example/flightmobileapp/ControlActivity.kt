cff861637c1dce6fbfb43e323abca5e9bb0f0d6c
danak1997 <danathekraimer@gmail.com>
2020-06-21 08:42:22 +0300

    add joystick and sliders and start of server requests

--- FlightMobileAppClient/app/src/main/java/com/example/flightmobileapp/ControlActivity.kt
+++ FlightMobileAppClient/app/src/main/java/com/example/flightmobileapp/ControlActivity.kt
@@ -2,10 +2,27 @@ package com.example.flightmobileapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
 import io.github.controlwear.virtual.joystick.android.JoystickView
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class ControlActivity : AppCompatActivity() {
+    private var currAilaroen: Double = 0.0
+    private var currThrottle: Double = 0.0
+    private var currRudder: Double = 0.0
+    private var currElevator: Double = 0.0
+
+    private var lastAilaroen: Double = 0.0
+    private var lastThrottle: Double = 0.0
+    private var lastRudder: Double = 0.0
+    private var lastElevator: Double = 0.0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.control_screen)
@@ -14,4 +31,55 @@ class ControlActivity : AppCompatActivity() {
             // do whatever you want
         }
     }
+
+    fun sendCommand() {
+        var url : String = "http://10.0.2.2:SERVER_PORT"
+        val jsonToSend: String = "{\"aileron\": $currAilaroen,\n \"rudder\": $currRudder, \n " +
+                "\"elevator\": $currElevator, \n \"throttle\": $currThrottle\n}"
+
+        val requestBody: RequestBody =
+            RequestBody.create(MediaType.parse("application/json"), jsonToSend)
+
+        val gson = GsonBuilder().setLenient().create()
+
+        val retrofit =
+            Retrofit
+                .Builder()
+                .baseUrl(url.toString())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+
+        val api = retrofit.create(Api::class.java)
+        val resBody = api.postCommand(requestBody).enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    fun getScreenShot() {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:SERVER_PORT/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+        val api = retrofit.create(Api::class.java)
+
+        val body = api.getImg().enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
 }
